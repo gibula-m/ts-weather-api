d7cacf87441a1f73fe1e6b4ce19e055036a2b251
Mateusz <mateusz@Mateuszs-MacBook-Air.local>
2020-06-18 14:52:22 +0200

    ci.yaml

--- tests/error.test.ts
+++ tests/error.test.ts
@@ -4,7 +4,7 @@ describe('Classes tests', () => {
   const err = new HttpError(500,'Internal error');
 
   it('HttpError class', async () => {
-    expect(err.statusCode).toBe(501);
+    expect(err.statusCode).toBe(500);
     expect(err.message).toBe('Internal error');
   });
 });
