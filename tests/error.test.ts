66ba5ae66fc123dec7e130e0ac4f14a7e7402945
Mateusz <mateusz@Mateuszs-MacBook-Air.local>
2020-06-18 14:28:00 +0200

    add tests structure to classes

---
+++ tests/error.test.ts
@@ -0,0 +1,10 @@
+import { HttpError } from '../src/class/HttpError';
+
+describe('Classes tests', () => {
+  const err = new HttpError(500,'Internal error');
+
+  it('HttpError class', async () => {
+    expect(err.statusCode).toBe(500);
+    expect(err.message).toBe('Internal error');
+  });
+});
